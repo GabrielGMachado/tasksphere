750e61d7910459d0ae3e7344351bc72825481ffc
Gabriel Machado <bielgaelzermachado@gmail.com>
2024-12-22 00:20:15 -0300

    finalize-design-of-the-home-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Almarai } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const almarai = Almarai({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Escolha os pesos que deseja usar
 });
 
 export default function RootLayout({
@@ -20,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-zinc-500 ${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen flex justify-center`}>
+        className={`bg-zinc-500 antialiased w-screen h-screen flex justify-center ${almarai.className}`}>
         <div className="max-w-md w-full">
           {children}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,57 @@
 import Image from "next/image";
-import Navbar from "./components/navbar";
 import Link from "next/link";
-import { Facebook } from "lucide-react";
-
+import { Apple, Facebook } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="bg-slate-900  h-full w-full">
+    <div className="bg-slate-900 h-full w-full">
       <div className="flex flex-col items-center justify-center h-full">
-        <div className="flex flex-col items-center mb-40 text-white">
-        <Image src="/logo.png" className="" width={80} height={80} alt="Logo TaskSphere"/>
-        <div className="flex flex-col items-center gap1">
-          <h1>TaskSphere</h1>
-          <p>Seu aplicativo de tarefas</p>
-        </div>
-        </div>
-        <div className="absolute max-w-md bottom-0 h-96 w-full bg-white rounded-tr-xl rounded-tl-xl p-5">
-          <h1 className="text-2xl">Entrar com </h1>
-          <div className="flex justify-between">
-            <button className="border border-black max-w-36 w-full">
-              <Link className="flex gap-2" href="/"> <Facebook/> Google</Link>
-            </button>
-            <button className="border border-black max-w-36 w-full">
-              <Link href="/" className=" flex gap-2"> <Facebook/> Facebook </Link>
-            </button>
+        <div className="flex flex-col items-center mb-44 text-white">
+          <Image src="/logo.png" width={80} height={80} alt="Logo TaskSphere" />
+          <div className="flex flex-col items-center mt-2">
+            <h1 className="text-3xl">TaskSphere</h1>
+            <p className="text-zinc-400">Seu aplicativo de tarefas</p>
           </div>
         </div>
+
+        <div className="absolute max-w-md bottom-0 h-96 w-full bg-white rounded-tr-xl rounded-tl-xl p-5">
+          <h1 className="text-2xl font-medium">Entrar com</h1>
+
+          <div className="flex justify-between mt-5 gap-4">
+            <Link href="/" className="border border-black w-full flex justify-center py-3.5 rounded-xl">
+              <button className="flex gap-2 font-bold" >
+                <Facebook /> Google
+              </button>
+            </Link> 
+
+            <Link href="/" className="border border-black w-full flex justify-center py-3.5 rounded-xl">
+              <button className="flex gap-2 font-bold" >
+              <Apple /> Apple
+              </button>
+            </Link> 
+          </div>
+
+          <div className="flex justify-between items-center mt-10 mb-10">
+            <div className="h-[1px] w-full bg-slate-900"></div>
+            <p className="px-4">Ou</p>
+            <div className="h-[1px] w-full bg-slate-900"></div>
+          </div>
+
+          <form className="flex flex-col gap-2">
+            <input
+              type="email"
+              placeholder="Email..."
+              className="border border-slate-700/60 rounded-xl w-full p-4 py-3 outline-none"
+              aria-label="Digite seu email"
+            />
+            <input
+              type="submit"
+              value="Continuar"
+              className="border text-white font-medium bg-slate-900 rounded-xl w-full p-2 py-3"
+            />
+          </form>
+        </div>
       </div>
-      
-   
-    
     </div>
   );
 }
